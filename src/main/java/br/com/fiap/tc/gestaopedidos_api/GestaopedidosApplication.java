c0984c9d5b549edbf93ec176f68a0b3f6ca8de67
MarcosHRFerreira <marcoshrferreira@gmail.com>
2024-11-01 09:11:46 -0300

    carga inicial

---
+++ src/main/java/br/com/fiap/tc/gestaopedidos_api/GestaopedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.gestaopedidos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaopedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaopedidosApplication.class, args);
+	}
+
+}
